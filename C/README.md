124adfaea295f3bdbc2a68493c42bc99427a301c
wenjiazheng1generation <419665732@qq.com>
2022-10-06 19:21:32 +0800

    123

---
+++ C/README.md
@@ -0,0 +1,2 @@
+# C_learning
+The learning process for C language
---
+++ C/test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int length, value;
+cin >> length >> value;
+cout<<length<<value<<endl;
